621274ec153523e2f7de2bc98de7244d3d1993b1
leonardobfn <75333445+leonardobfn@users.noreply.github.com>
2024-02-07 16:18:34 -0400

    atualização

---
+++ R/nascido_vivo_am_2022_DATASUS.R
@@ -0,0 +1,22 @@
+#' Nascidos vivos no Estado do Amazonas no ano de 2022
+#'
+#' Base de dados de nascidos vivos no Estado do Amazonas no ano de 2022
+#'
+#' @format
+#' Um data frame com 62 linhas e 3 colunas:
+#' \describe{
+#'   \item{Município}{Código e nomes dos municípios do Estado do Amazonas}
+#'   \item{Nascim_p_resid_mãe}{Número de nascidos vivos, contados segundo o local de residência da mãe.}
+#'   \item{Nascim_p_ocorrência}{Número de nascidos vivos, contados segundo o local de ocorrência do nascimento.}
+#'   ...
+#' }
+#' @source A base de dados foi acessada no dia 30/01/2024 em <http://tabnet.datasus.gov.br/cgi/deftohtm.exe?sinasc/cnv/nvam.def>
+#' @references Descrições das variáveis <http://tabnet.datasus.gov.br/cgi/sinasc/Nascidos_Vivos_1994_2012.pdf>
+#' @docType data
+#' @keywords datasets
+#' @name nascido_vivo_am_2022_DATASUS
+#' @usage data(nascido_vivo_am_2022_DATASUS)
+#' @examples
+#' data(nascido_vivo_am_2022_DATASUS)
+#' head(nascido_vivo_am_2022_DATASUS)
+"nascido_vivo_am_2022_DATASUS"
---
+++ testes_pkg.R
@@ -0,0 +1,17 @@
+
+
+devtools::document() #o generate (or update) your package’s .Rd files
+roxygen2::roxygenise() # gerar documento help
+devtools::load_all() #  tests interactively in the console
+devtools::install() # instala o pacote
+
+usethis::use_testthat()
+usethis::use_test("R")
+
+devtools::test()
+devtools::check()
+
+?nascido_vivo_am_2022_DATASUS
+
+
+
